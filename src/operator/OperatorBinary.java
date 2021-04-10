0445f0315c32b94ad92890ceeae36b0e703c5bc7
Ritapurnamasarii <ritapurnamasarii02@gmail.com>
2021-04-10 13:46:40 +0800

    Operator Binary Assigment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,13 @@
+package operator;
+
+public class OperatorBinary  {
+
+    //operator (=) sebagia assignment/penugasan
+    //menempatkan nilai kedalam variable
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = "Rita Purnamasari";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.89;
+
+}
