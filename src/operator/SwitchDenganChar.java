95effafc3a7291d8f8ef28ed9f82065122c491f1
Ritapurnamasarii <ritapurnamasarii02@gmail.com>
2021-04-10 14:56:19 +0800

    Switch char percakapan Mhs dan Dosen

---
+++ src/operator/SwitchDenganChar.java
@@ -0,0 +1,29 @@
+package operator;
+
+public class SwitchDenganChar {
+    public static void main(String[] args) {
+        char nilai='D';
+        switch(nilai){
+            case 'A':
+                System.out.println("Mhs   : \"Terima kasih pak\"");
+                System.out.println("Dosen : \"Selamat ya !!\"");
+                break;
+            case 'B':
+                System.out.println("Mhs   : \"Kenpa ulun kada dapat A, pak\"");
+                System.out.println("Dosen : \"!@#$%\"");
+                break;
+            case 'C':
+                System.out.println("Mhs   : \"Ulun turun pul Pak ai, tugas pu jua\"");
+                System.out.println("Dosen : \"Tapi bisa lah menjawab ujian?\"");
+                System.out.println("Mhs   : \"Hihihi..\"");
+                break;
+            default:
+                System.out.println("Mhs   : \"Ulun turun pul Pak ai, tugas pul jua\"");
+                System.out.println("Dosen : \"Bujur jua kah?\"");
+                System.out.println("Dosen : \"*memeriksa berkas\"");
+                System.out.println("Dosen : \"*menceleng\"");
+                System.out.println("Mhs   : \"Kabur..\"");
+                break;
+        }
+    }
+}
